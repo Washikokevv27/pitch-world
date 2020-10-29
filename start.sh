ae3304a1d4e37339455cbe23aee202192275c887
Washikokevv27 <washikokevv27@gmail.com>
2020-10-29 16:41:37 +0300

    first commit

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY=18511174a077429aaaeaee76c5574ee9
+export MAIL_USERNAME=washikokevv27@gmail.com
+export MAIL_PASSWORD=moringa123
+
+export SQLALCHEMY_DATABASE_URI ='postgres://moringa:0000@localhost:5432/pitches'
